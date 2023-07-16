2a731caea3b391c7f67fc4050b588df2974966dd
Hemanth kumar <94438418+Hemanthkumar335@users.noreply.github.com>
2023-07-16 11:07:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+let pic = document.getElementById('i2');
+function mb(){
+    pic.style.cssText = "margin-left: 150px;margin-top: 150px; box-shadow: 6px 6px 6px 6px black; transform: scale(3); transition: 2s; display: inline;"
+}
